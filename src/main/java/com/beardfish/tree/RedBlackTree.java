e8af4b5f4c83bf37432925a21451545732f1fac1
Christian Bernard <masterblah777@gmail.com>
2014-03-06 21:43:18 -0700

    Removed searchTree* methods because they were not performing the correct function. Added binary search methods to add a node to the tree. Added rebalanceTree starting at a specific node and working up the tree. Implementation for rebalance tree is not finished yet

--- src/main/java/com/beardfish/tree/RedBlackTree.java
+++ src/main/java/com/beardfish/tree/RedBlackTree.java
@@ -15,185 +15,179 @@ import java.util.Iterator;
 import java.util.Set;
 
 public class RedBlackTree<E> implements Set<E> {
-	
-	private final Comparator<? super E> comparator;
-	private int size = 0;
-	private Node<E> root = null;
-	
-	public RedBlackTree(Comparator<? super E> comparator) {
-		this.comparator = comparator;
-	}
-	
 
-	@Override
-	public boolean add(E ele) {
-		if(this.root==null) {
-			this.root = new Node<E>(ele,null,null,null);
+    private final Comparator<? super E> comparator;
+    private int size = 0;
+    private Node<E> root = null;
+
+    public RedBlackTree(Comparator<? super E> comparator) {
+        this.comparator = comparator;
+    }
+
+
+    @Override
+    public boolean add(E ele) {
+        if (this.root == null) {
+            this.root = new Node<E>(ele, null, null, null);
             this.root.setColor(Node.Color.Black);
-		} else {
-			if(this.comparator!=null) {
-                this.searchInTreeWithComparator(this.root,ele);
+        } else {
+            if (this.comparator != null) {
+                this.binarySearchWithComparator(this.root, ele);
             } else {
-                this.searchInTreeWithComparable(this.root,ele);
+                this.binarySearchWithComparable(this.root, ele);
             }
-		}
-		return false;
-	}
+        }
+        return false;
+    }
 
-	@Override
-	public boolean addAll(Collection<? extends E> col) {
-		return false;
-	}
+    @Override
+    public boolean addAll(Collection<? extends E> col) {
+        return false;
+    }
 
     /**
-     * Search downward in the tree using comparable until the appropriate node is found
-     * @param node
+     * Adds a node into the tree using the comparator
+     *
+     * @param node    -- the starting node
+     * @param element -- the element to be added
      */
-    public void searchInTreeWithComparable(Node<E> node, E element) {
+    public void binarySearchWithComparator(Node<E> node, E element) {
+        int compare = this.comparator.compare(element, node.getValue());
+        if (compare < 0) {
+            /* element is less than node value */
+            if (node.getLeft() != null) {
+                binarySearchWithComparator(node.getLeft(), element);
+            } else {
+                node.setLeft(new Node<E>(element, node, null, null));
+            }
+        } else if (compare > 0) {
+            /* element is greater than node value */
+            if (node.getRight() != null) {
+                binarySearchWithComparator(node.getRight(), element);
+            } else {
+                node.setRight(new Node<E>(element, node, null, null));
+            }
+        }
+    }
+
+    /**
+     * Adds a node into the tree using the comparable
+     *
+     * @param node    -- the starting node
+     * @param element -- the element to be added
+     */
+    public void binarySearchWithComparable(Node<E> node, E element) {
         Comparable<? super E> key = (Comparable<? super E>) element;
         int compare = key.compareTo(node.getValue());
-        if(compare<0) {
+        if (compare < 0) {
             /* element is less than node value */
-            if(node.getLeft()!=null) {
-                searchInTreeWithComparable(node.getLeft(),element);
+            if (node.getLeft() != null) {
+                binarySearchWithComparable(node.getLeft(), element);
             } else {
-                /* if the parent is black its okay to add a red child */
-                if(node.getColor().equals(Node.Color.Black)) {
-                    node.setLeft(new Node<E>(element,node,null,null));
-                } else if(node.getColor().equals(Node.Color.Red)) {
-                    /* check the uncle to see if he is red as well */
-                    Node<E> uncle = (Node<E>) getUncle(node);
-                    if(uncle.getColor().equals(Node.Color.Red)) {
-
-                    }
-                }
+                node.setLeft(new Node<E>(element, node, null, null));
             }
-        } else if(compare>0) {
+        } else if (compare > 0) {
             /* element is greater than node value */
-            if(node.getRight()!=null) {
-                searchInTreeWithComparable(node.getRight(),element);
+            if (node.getRight() != null) {
+                binarySearchWithComparable(node.getRight(), element);
             } else {
-                /* if the parent is black its okay to add a red child */
-                if(node.getColor().equals(Node.Color.Black)) {
-                    node.setRight(new Node<E>(element,node,null,null));
-                } else if(node.getColor().equals(Node.Color.Red)) {
-                    /* check the uncle to see if he is red as well */
-                    Node<E> uncle = (Node<E>) getUncle(node);
-                    if(uncle.getColor().equals(Node.Color.Red)) {
-
-                    }
-                }
+                node.setRight(new Node<E>(element, node, null, null));
             }
         }
     }
 
-    /** Search downard in the tree using the comparator until the appropriate node is found
-     * @param node
+    /**
+     * Rebalance the tree until all properties are fullfilled
+     * @param node -- the node to start the rebalance from
      */
-    public void searchInTreeWithComparator(Node<E> node,E element) {
-        int compare = this.comparator.compare(element,node.getValue());
-        if(compare<0) {
-            /* element is less than node value */
-            if(node.getLeft()!=null) {
-                searchInTreeWithComparator(node.getLeft(),element);
-            } else {
-                /* if the parent is black its okay to add a red child */
-                if(node.getColor().equals(Node.Color.Black)) {
-                    node.setLeft(new Node<E>(element,node,null,null));
-                } else if(node.getColor().equals(Node.Color.Red)) {
-                    /* check the uncle to see if he is red as well */
-                    Node<E> uncle = (Node<E>) getUncle(node);
-                    if(uncle.getColor().equals(Node.Color.Red)) {
-
-                    }
-                }
-            }
-        } else if (compare>0) {
-            /* element is greater than node value*/
-            if(node.getRight()!=null) {
-                searchInTreeWithComparator(node.getRight(),element);
-            } else {
-                /* if the parent is black then its okay to add a red child */
-                if(node.getColor().equals(Node.Color.Black)) {
-                    node.setLeft(new Node<E>(element,node,null,null));
-                } else if(node.getColor().equals(Node.Color.Red)) {
-                    /* check the uncle to see if he is red as well */
-                    Node<E> uncle = (Node<E>) getUncle(node);
-                    if(uncle.getColor().equals(Node.Color.Red)) {
-
-                    }
-                }
+    public void rebalanceTree(Node<E> node) {
+        /* root */
+        if(node.getParent()==null) {
+            node.setColor(Node.Color.Black);
+        }
+        /* if the parent is black its okay to add a red child */
+        if (node.getParent().getColor().equals(Node.Color.Black)) {
+            return;
+        } else if (node.getParent().getColor().equals(Node.Color.Red)) {
+            /* check the uncle to see if he is red as well */
+            Node<E> uncle = (Node<E>) getUncle(node);
+            if ((uncle != null) && uncle.getColor().equals(Node.Color.Red)) {
+                node.getParent().setColor(Node.Color.Black);
+                uncle.setColor(Node.Color.Black);
+                Node<E> grandParent = (Node<E>) getGrandParent(node);
+                grandParent.setColor(Node.Color.Red);
+                rebalanceTree(grandParent);
             }
         }
     }
 
 
-	@Override
-	public void clear() {
+    @Override
+    public void clear() {
 
-	}
+    }
 
-	@Override
-	public boolean contains(Object o) {
+    @Override
+    public boolean contains(Object o) {
         return false;
-	}
+    }
 
-	@Override
-	public boolean containsAll(Collection<?> col) {
-		
-		return false;
-	}
+    @Override
+    public boolean containsAll(Collection<?> col) {
 
-	@Override
-	public boolean isEmpty() {
-		
-		return size==0;
-	}
+        return false;
+    }
 
-	@Override
-	public Iterator<E> iterator() {
-		
-		return null;
-	}
+    @Override
+    public boolean isEmpty() {
 
-	@Override
-	public boolean remove(Object o) {
-		
-		return false;
-	}
+        return size == 0;
+    }
 
-	@Override
-	public boolean removeAll(Collection<?> col) {
-		
-		return false;
-	}
+    @Override
+    public Iterator<E> iterator() {
 
-	@Override
-	public boolean retainAll(Collection<?> col) {
-		
-		return false;
-	}
+        return null;
+    }
 
-	@Override
-	public int size() {
-		
-		return this.size;
-	}
+    @Override
+    public boolean remove(Object o) {
 
-	@Override
-	public Object[] toArray() {
-		
-		return null;
-	}
+        return false;
+    }
 
-	@Override
-	public <T> T[] toArray(T[] arr) {
-		
-		return null;
-	}
+    @Override
+    public boolean removeAll(Collection<?> col) {
+
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> col) {
+
+        return false;
+    }
+
+    @Override
+    public int size() {
+
+        return this.size;
+    }
+
+    @Override
+    public Object[] toArray() {
+
+        return null;
+    }
+
+    @Override
+    public <T> T[] toArray(T[] arr) {
+
+        return null;
+    }
 
     private static Node<?> getGrandParent(Node<?> node) {
-        if(node!=null && node.getParent()!=null) {
+        if (node != null && node.getParent() != null) {
             node.getParent().getParent();
         }
         return null;
@@ -201,40 +195,42 @@ public class RedBlackTree<E> implements Set<E> {
 
     private static Node<?> getUncle(Node<?> node) {
         Node<?> grandParent = getGrandParent(node);
-        if(grandParent==null) {
+        if (grandParent == null) {
             return null;
         }
-        if(node.getParent()==grandParent.getLeft()) {
+        if (node.getParent() == grandParent.getLeft()) {
             return grandParent.getRight();
         }
         return grandParent.getLeft();
     }
-	
-	private static class Node<E> {
 
-        public static enum Color {Black,Red};
-		
-		private final E value;
+    private static class Node<E> {
+
+        public static enum Color {Black, Red}
+
+        ;
+
+        private final E value;
         private Node<E> parent;
-		private Node<E> left;
-		private Node<E> right;
-		private Color color;
+        private Node<E> left;
+        private Node<E> right;
+        private Color color;
 
         public Node(E value, Node<E> parent, Node<E> left, Node<E> right) {
-            this(value,parent,left,right,Color.Red);
+            this(value, parent, left, right, Color.Red);
         }
 
-		public Node(E value, Node<E> parent, Node<E> left, Node<E> right, Color color) {
-			this.value = value;
+        public Node(E value, Node<E> parent, Node<E> left, Node<E> right, Color color) {
+            this.value = value;
             this.parent = parent;
-			this.left = left;
-			this.right = right;
+            this.left = left;
+            this.right = right;
             this.color = color;
-		}
-		
-		public E getValue() {
-			return value;
-		}
+        }
+
+        public E getValue() {
+            return value;
+        }
 
         public Node<E> getParent() {
             return this.parent;
@@ -243,30 +239,30 @@ public class RedBlackTree<E> implements Set<E> {
         public void setParent(Node<E> parent) {
             this.parent = parent;
         }
-		
-		public Node<E> getLeft() {
-			return left;
-		}
+
+        public Node<E> getLeft() {
+            return left;
+        }
 
         public void setLeft(Node<E> left) {
             this.left = left;
         }
-		
-		public Node<E> getRight() {
-			return right;
-		}
+
+        public Node<E> getRight() {
+            return right;
+        }
 
         public void setRight(Node<E> right) {
             this.right = right;
         }
-		
-		public Color getColor() {
-			return this.color;
-		}
-		
-		public void setColor(Color color) {
-			this.color = color;
-		}
-	}
+
+        public Color getColor() {
+            return this.color;
+        }
+
+        public void setColor(Color color) {
+            this.color = color;
+        }
+    }
 
 }
