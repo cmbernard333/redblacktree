2da947dfa546b1c5e340366ab47e4add9eb1cffe
Christian Bernard <masterblah777@gmail.com>
2014-03-23 13:15:20 -0600

    RedBlackTree.java ran intellj format. Changed binarySearch method to return the node that it finds or null if it doesn't find it. Added the initial implementation of remove that can handle removing the root. Added implementation of removeAll. Added breadthFirstSearch method to do level order printing. Added sibling method to Node class.

--- src/test/java/com/beardfish/tree/test/RedBlackTreeTest.java
+++ src/test/java/com/beardfish/tree/test/RedBlackTreeTest.java
@@ -12,46 +12,58 @@ import org.junit.Test;
  */
 public class RedBlackTreeTest {
 
-  @Test
-  public void testRedBlackTreeInsert() {
-    RedBlackTree<Integer> rbt = new RedBlackTree<Integer>();
-    for (int i = 1; i < 9; i++) {
-      rbt.add(i);
+    @Test
+    public void testRedBlackTreeInsert() {
+        RedBlackTree<Integer> rbt = new RedBlackTree<Integer>();
+        for (int i = 1; i < 9; i++) {
+            rbt.add(i);
+        }
+        Iterator<Integer> it = rbt.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+
     }
-    Iterator<Integer> it = rbt.iterator();
-    while(it.hasNext()) {
-      System.out.println(it.next());
+
+
+    @Test
+    public void depthFirstSearchTest() {
+        RedBlackTree<Integer> rbt = new RedBlackTree<Integer>();
+        System.out.println("Depth First Search:");
+        for (int i = 1; i < 11; i++) {
+            rbt.add(i);
+        }
+        rbt.depthFirstSearch();
     }
-    
-  }
-  
-  
-  @Test
-  public void depthFirstSearchTest() {
-    RedBlackTree<Integer> rbt = new RedBlackTree<Integer>();
-    for(int i = 1; i < 11; i++) {
-      rbt.add(i);
+
+    @Test
+    public void breadthFirstSearchTest() {
+        RedBlackTree<Integer> rbt = new RedBlackTree<Integer>();
+        System.out.println("Breadth First Search: ");
+        for (int i =1; i <11 ;i++) {
+            rbt.add(i);
+        }
+        rbt.breadthFirstSearch();
     }
-    rbt.depthFirstSearch();
-  }
-  
-  @Test
-  public void characterContainsTest() {
-	  RedBlackTree<Character> rbt = new RedBlackTree<Character>();
-	  char arr [] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-	  for(int i = 0; i<arr.length;i++) {
-		  rbt.add(arr[i]);
-	  }
-	  for(int i = 0; i<arr.length;i++) {
-		  Assert.assertTrue(rbt.contains(new Character(arr[i])));
-	  }
-	  
-  }
-  
-  @Test
-  public void integerContainsTestFalse() {
-    RedBlackTree<String> rbt = new RedBlackTree<String>();
-    Assert.assertFalse(rbt.contains("wombat"));
-  }
+
+
+    @Test
+    public void characterContainsTest() {
+        RedBlackTree<Character> rbt = new RedBlackTree<Character>();
+        char arr[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        for (int i = 0; i < arr.length; i++) {
+            rbt.add(arr[i]);
+        }
+        for (int i = 0; i < arr.length; i++) {
+            Assert.assertTrue(rbt.contains(new Character(arr[i])));
+        }
+
+    }
+
+    @Test
+    public void integerContainsTestFalse() {
+        RedBlackTree<String> rbt = new RedBlackTree<String>();
+        Assert.assertFalse(rbt.contains("wombat"));
+    }
 
 }
