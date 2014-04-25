082616c4a18680ea848f60b2671c3061083315c9
Christian Bernard <masterblah777@gmail.com>
2014-04-25 13:43:42 -0600

    Added randomInsertTest. Changed length of depthFirstSearchTest to 9

--- src/test/java/com/beardfish/tree/test/RedBlackTreeTest.java
+++ src/test/java/com/beardfish/tree/test/RedBlackTreeTest.java
@@ -25,12 +25,23 @@ public class RedBlackTreeTest {
 
     }
 
+    @Test
+    public void randomInsertTest() {
+        RedBlackTree<Integer> rbt = new RedBlackTree<Integer>();
+        int [] numbers = {13,8,17,1,11,15,25,6,22,27};
+        for(int i = 0; i <numbers.length;i++) {
+            rbt.add(numbers[i]);
+        }
+        /* print in order */
+        rbt.depthFirstSearch();
+    }
+
 
     @Test
     public void depthFirstSearchTest() {
         RedBlackTree<Integer> rbt = new RedBlackTree<Integer>();
         System.out.println("Depth First Search:");
-        for (int i = 1; i < 11; i++) {
+        for (int i = 1; i < 9; i++) {
             rbt.add(i);
         }
         rbt.depthFirstSearch();
